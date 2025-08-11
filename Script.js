d5c2a054d8146bd4842f0f4febf9b369e75d79be
Abhishek kumar <136875844+Abhisheksaw@users.noreply.github.com>
2025-08-12 01:34:42 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -42,17 +42,17 @@ window.addEventListener("scroll", () => {
 const mainCarouselData = [
   {
     image:
-      "https://www.ethnicplus.in/cdn/shop/files/4.jpg?v=1752084367&width=2000",
+      "https://www.koskii.com/cdn/shop/files/RM-Desktop_dac9eecc-a483-4a07-beac-3a469b1e0f0e.jpg?v=1754918824&width=1920",
     path: "/women/clothing/lengha_choli",
   },
   {
     image:
-      "https://www.ethnicplus.in/cdn/shop/files/1_03ac5260-7141-4676-b1f8-d8318df825a4.jpg?v=1751950894&width=1500",
+      "https://www.koskii.com/cdn/shop/files/DM--Desktop_1.jpg?v=1754918825&width=1920",
     path: "/women/clothing/women_dress",
   },
   {
     image:
-      "https://www.ethnicplus.in/cdn/shop/files/3_37951304-c08e-44c9-874e-c66b063dc544.jpg?v=1752084367&width=2000",
+      "https://www.koskii.com/cdn/shop/files/Saree--Desktop.jpg?v=1754918825&width=1920",
     path: "/women/clothing/women_dress",
   },
   {
